092f4fa50a454f5f14dcc35f6ee07ff7c9f11d36
Arslan Mushtaq <arslanark@gmail.com>
2022-06-20 17:01:43 +0500

    add routing and game room

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GameRoom from "pages/GameRoom";
+import Home from "pages/Home";
+import { Route, Routes } from "react-router-dom";
+import { routes } from "shared";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path={routes.HOME} element={<Home />} />
+      <Route path={routes.PLAY} element={<GameRoom />} />
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ src/shared/routes.ts
@@ -0,0 +1,4 @@
+export const routes = {
+  HOME: "/",
+  PLAY: "/play/:roomId/:gameId",
+};
